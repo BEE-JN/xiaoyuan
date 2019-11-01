b226224304204789e7a45ef1fcd59dee287718cb
Lin Xuanran <haohaoruyan@gmail.com>
2019-11-01 10:55:10 +0800

    修改了readme的更新

---
+++ src/main/java/com/jngcs/xiaoyuan/XiaoyuanApplication.java
@@ -0,0 +1,13 @@
+package com.jngcs.xiaoyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoyuanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoyuanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jngcs/xiaoyuan/XiaoyuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jngcs.xiaoyuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaoyuanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
